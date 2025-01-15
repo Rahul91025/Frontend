f19d6bcf095cda86f0744145323a39285b4dc16b
Rahul Gupta <126942167+Rahul91025@users.noreply.github.com>
2025-01-15 22:42:54 +0530

    Update Profile.jsx

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -252,7 +252,7 @@ function AccountPage() {
           <div className="mt-4 text-center">
             <button
               onClick={handleEditToggle}
-              className="bg-blue-500 text-white px-4 py-2 rounded"
+              className="bg-black text-white px-4 py-2 rounded"
             >
               {isEditing ? "Save Changes" : "Edit Profile"}
             </button>
